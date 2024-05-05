8a38c46da9305edc14229e23aeb30ad803c2afbb
SlaviHristev <Slavihristev97@gmail.com>
2024-05-05 11:32:23 +0300

    Going to use react-router-dom and added some more pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import Navbar from './components/navbar/Navbar'
+
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Layout from './pages/layout/Layout'
 import Home from './pages/home/Home'
-import './layout.scss'
+import List from './pages/list/List'
+import Login from './pages/login/Login'
+import SinglePage from './pages/singlePage/SinglePage'
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:(
+        <Layout/>
+      ),
+      children:[
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+          path:'/list',
+          element:<List/>
+        },
+        {
+          path:'/login',
+          element:<Login/>
+        },
+        {
+          path:'/:id',
+          element:<SinglePage/>
+        }
+      ]
+    },
+  ])
+
   return (
-    <div className="layout">
-      <div className='navbar'>
-      <Navbar/>
-
-      </div>
-      <div className="content">
-      <Home/>
-
-      </div>
-    </div>
+    
+    <RouterProvider router={router}/>
   )
 }
 
