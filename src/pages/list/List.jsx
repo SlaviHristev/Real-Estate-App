9ca97471d292c3e96ffe68fd7579b2edc8dd13eb
SlaviHristev <Slavihristev97@gmail.com>
2024-05-05 12:16:15 +0300

    Started working on the list page and created card and filter  components for the page and starting to work on them

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,26 @@
+import Card from '../../components/card/Card';
+import Filter from '../../components/filter/Filter';
+import {listData} from '../../lib/tempData';
+
 import './list.scss'
 
 const List = () => {
+
+    const data = listData;
+
   return (
     <div className='list'>
-        
+        <div className="listContainer">
+            <div className="wrapper">
+                <Filter/>
+                {data.map(item =>(
+                    <Card key={item.id} item={item}/>
+                ))}
+            </div>
+        </div>
+        <div className="mapContainer">
+
+        </div>
     </div>
   )
 }
