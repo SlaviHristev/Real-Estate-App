082fb0c5e514dbec7f2a051ccae1c84ef059f3fa
SlaviHristev <Slavihristev97@gmail.com>
2024-05-11 17:23:53 +0300

    Styled the profile page and now working on the chat component

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,41 @@
+import './chat.scss'
+
+const Chat = () => {
+  return (
+    <div className='chat'>
+        <div className="messages">
+            <h1>Messages</h1>
+            <div className="message">
+                <img src="/logo.png" alt="" />
+                <span>Pesho</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, enim.</p>
+            </div>
+            <div className="message">
+                <img src="/logo.png" alt="" />
+                <span>Pesho</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, enim.</p>
+            </div>
+            <div className="message">
+                <img src="/logo.png" alt="" />
+                <span>Pesho</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, enim.</p>
+            </div>
+            <div className="message">
+                <img src="/logo.png" alt="" />
+                <span>Pesho</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, enim.</p>
+            </div>
+            <div className="message">
+                <img src="/logo.png" alt="" />
+                <span>Pesho</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, enim.</p>
+            </div>
+        </div>
+        <div className="chatBox">
+
+        </div>
+    </div>
+  )
+}
+
+export default Chat
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,3 +1,4 @@
+import Chat from '../../components/chat/Chat'
 import List from '../list/List'
 import './profile.scss'
 
@@ -27,7 +28,9 @@ const Profile = () => {
             </div>
         </div>
         <div className="chatContainer">
-            <div className="wrapper"></div>
+            <div className="wrapper">
+                <Chat/>
+            </div>
         </div>
     </div>
   )
