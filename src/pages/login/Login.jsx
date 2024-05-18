e7a95f38659d36f3458d55d9ac4f88a47bc89b9b
SlaviHristev <Slavihristev97@gmail.com>
2024-05-18 11:39:51 +0300

    Created the login and register pages

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
 import './login.scss'
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className="login">
+      <div className="formContainer">
+        <form>
+          <h1>Welcome back</h1>
+          <input name="username" required minLength={3} maxLength={20} type="text" placeholder="Username"/>
+          <input name="password" type="password" requiredplaceholder="Password"/>
+          <button>Login</button>
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
   )
 }
 
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,24 @@
+import './register.scss';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  return (
+    <div className='register'>
+        <div className="formContainer">
+            <form>
+                <h1>Create an Account</h1>
+                <input type="text" name="username" placeholder='Username' />
+                <input type="text" name="email" placeholder='Email' />
+                <input type="password" name="password" placeholder='Password' />
+                <button>Register</button>
+                <Link to='/login'>Already have an account?</Link>
+            </form>
+        </div>
+        <div className="imgContainer">
+            <img src="/bg.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Register;
