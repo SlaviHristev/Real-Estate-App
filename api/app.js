6116fce54eb4683457aec2b78cacf5fa56f21db5
SlaviHristev <Slavihristev97@gmail.com>
2024-05-18 15:23:19 +0300

    Started working on authorization and protected routes

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import postRoute from './routes/post.js';
 import authRoute from './routes/auth.js';
 import userRoute from './routes/user.js';
+import testRoute from './routes/test.js';
 import cors from 'cors';
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use('/api/post', postRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
+app.use('/api/test', testRoute);
 
 app.listen(8800, () => {
     console.log('Server is running on port 8880!');
