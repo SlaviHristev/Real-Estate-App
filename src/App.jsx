cb9afd85311282868be696bf49ec5453cb96914e
SlaviHristev <Slavihristev97@gmail.com>
2024-05-25 10:50:22 +0300

    Finished styling my updateUser page

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,68 @@
 
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import  { Layout ,RequireAuth } from './pages/layout/Layout'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Layout, RequireAuth } from './pages/layout/Layout'
 import Home from './pages/home/Home'
 import List from './pages/list/List'
 import Login from './pages/login/Login'
 import SinglePage from './pages/singlePage/SinglePage'
 import Profile from './pages/profile/Profile'
 import Register from './pages/register/Register'
-import ProfileUpdate from './pages/profileUpdate/profileUpdate'
+import ProfileUpdate from './pages/profileUpdate/ProfileUpdate'
+
 
 
 function App() {
 
   const router = createBrowserRouter([
     {
-      path:'/',
-      element:(
-        <Layout/>
+      path: '/',
+      element: (
+        <Layout />
       ),
-      children:[
+      children: [
         {
-          path:'/',
-          element:<Home/>
+          path: '/',
+          element: <Home />
         },
         {
-          path:'/list',
-          element:<List/>
+          path: '/list',
+          element: <List />
         },
         {
-          path:'/login',
-          element:<Login/>
+          path: '/login',
+          element: <Login />
         },
         {
-          path:'/:id',
-          element:<SinglePage/>
+          path: '/:id',
+          element: <SinglePage />
         },
-       
+
         {
-          path:'/register',
-          element:<Register/>
+          path: '/register',
+          element: <Register />
         },
 
       ]
     },
     {
-      path:'/',
-      element: <RequireAuth/>,
-      children:[
+      path: '/',
+      element: <RequireAuth />,
+      children: [
         {
-          path:'/profile',
-          element:<Profile/>
+          path: '/profile',
+          element: <Profile />
         },
         {
-          path:'/profile/update',
-          element:<ProfileUpdate/>
+          path: '/profile/update',
+          element: <ProfileUpdate />
         },
       ]
     }
   ])
 
   return (
-    
-    <RouterProvider router={router}/>
+
+    <RouterProvider router={router} />
   )
 }
 
--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
 import './profileUpdate.scss'
+import { AuthContext } from '../../context/AuthContext';
+
 
 const ProfileUpdate = () => {
+    const {currentUser, updateUser} = useContext(AuthContext);
+
   return (
     <div className='profileUpdate'>
         <div className="formContainer">
@@ -8,11 +13,11 @@ const ProfileUpdate = () => {
                 <h1>Update Profile</h1>
                 <div className="item">
                     <label htmlFor="username">Username</label>
-                    <input type="text" name='username' id='username' />
+                    <input type="text" name='username' id='username' defaultValue={currentUser.username}/>
                 </div>
                 <div className="item">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name='email' id='email' />
+                    <input type="email" name='email' id='email' defaultValue={currentUser.email}/>
                 </div>
                 <div className="item">
                     <label htmlFor="password">Password</label>
@@ -22,7 +27,7 @@ const ProfileUpdate = () => {
             </form>
         </div>
         <div className="sideContainer">
-            <img src="" alt="" className='avatar' />
+            <img src={currentUser.avatar || '/noavatar.jpg'} alt="" className='avatar' />
         </div>
     </div>
   )
