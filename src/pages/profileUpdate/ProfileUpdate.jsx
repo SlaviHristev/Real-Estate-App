861174307ee10b178fe0b6ef7b4815393d89dc55
SlaviHristev <Slavihristev97@gmail.com>
2024-05-25 12:01:00 +0300

    Implemented the updating in the client side using the api

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,15 +1,33 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import './profileUpdate.scss'
 import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
+import { useNavigate } from 'react-router-dom';
 
 
 const ProfileUpdate = () => {
+    const [error, setError] = useState('');
     const {currentUser, updateUser} = useContext(AuthContext);
+    const navigate = useNavigate();
 
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        const {username, email, password} = Object.fromEntries(formData);
+        try {
+            const res = await apiRequest.put(`/users/${currentUser.id}`,{username,email,password});
+            updateUser(res.data);
+            navigate('/profile')
+        } catch (err) {
+            console.log(err);
+            setError(err.response.data.message)
+        }
+    }
   return (
     <div className='profileUpdate'>
         <div className="formContainer">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Update Profile</h1>
                 <div className="item">
                     <label htmlFor="username">Username</label>
@@ -24,6 +42,7 @@ const ProfileUpdate = () => {
                     <input type="password" name='password' id='password' />
                 </div>
                 <button>Update</button>
+                {error && <span>{error}</span>}
             </form>
         </div>
         <div className="sideContainer">
