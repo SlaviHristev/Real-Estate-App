e5abfe5a5282a4334cd650f958afe6b40e0b8613
SlaviHristev <Slavihristev97@gmail.com>
2024-06-09 19:23:38 +0300

    Continued working on the socket logic

--- socket/app.js
+++ socket/app.js
@@ -9,10 +9,26 @@ const io = new Server({
 
 let onlineUser = [];
 
+const addUser = (userId,socketId) =>{
+    const userExists = onlineUser.find((user) => user.userId === userId);
 
+    if(!userExists){
+        onlineUser.push({userId, socketId})
+    }
+}
 
+const removeUser = (socketId) =>{
+    onlineUser = onlineUser.filter((user) => user.socketId !== socketId)
+}
 io.on("connection", (socket) =>{
     
+    socket.on("newUser", (userId) =>{
+        addUser(userId, socket.id)
+    })
+
+    socket.on("disconnect", () =>{
+        removeUser(socket.id)
+    })
 });
 
 io.listen('4000');
